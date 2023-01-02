b8b88879e68327023d97e545b4e7dfd2cd292e50
Luke de Boer <lukedeboer48@gmail.com>
2023-01-02 20:39:26 +1000

    initial commit

---
+++ train.py
@@ -0,0 +1,71 @@
+import json
+from ntlk_utils import tokenize, stem, bag_of_words
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
+
+
+ignore_words = ["?", "!", ".", ","]
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
+print(tags)
+
+X_train = []
+y_train = []
+for (patter_sentence, tag) in xy:
+    bag = bag_of_words(patter_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label) # CrossEntropyLoss
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+
+class ChatDataSet(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    def __getitem__(self, item):
+        return self.x_data[idx], self.y_data[idx]
+
+    def __len__(self):
+        return self.n_samples
+
+
+# Hyperparameters
+batch_size = 8
+
+dataset = ChatDataSet()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+
+
+
+
+
+
+
+
+
